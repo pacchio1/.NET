f5271c6e5231d790a995455d244f8a9a2bac733c
Pacchiotto <marcopac.363.d@gmail.com>
2022-05-13 12:25:29 +0200

    Super Ping

---
+++ Super Ping/SuperPing/SuperPing/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SuperPing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.rdbOn = new System.Windows.Forms.RadioButton();
+            this.rdbOff = new System.Windows.Forms.RadioButton();
+            this.save = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "forza prima scansione";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // rdbOn
+            // 
+            this.rdbOn.AutoSize = true;
+            this.rdbOn.Location = new System.Drawing.Point(12, 353);
+            this.rdbOn.Name = "rdbOn";
+            this.rdbOn.Size = new System.Drawing.Size(39, 17);
+            this.rdbOn.TabIndex = 1;
+            this.rdbOn.TabStop = true;
+            this.rdbOn.Text = "On";
+            this.rdbOn.UseVisualStyleBackColor = true;
+            this.rdbOn.CheckedChanged += new System.EventHandler(this.Name_CheckedChanged);
+            // 
+            // rdbOff
+            // 
+            this.rdbOff.AutoSize = true;
+            this.rdbOff.Checked = true;
+            this.rdbOff.Location = new System.Drawing.Point(57, 353);
+            this.rdbOff.Name = "rdbOff";
+            this.rdbOff.Size = new System.Drawing.Size(39, 17);
+            this.rdbOff.TabIndex = 2;
+            this.rdbOff.TabStop = true;
+            this.rdbOff.Text = "Off";
+            this.rdbOff.UseVisualStyleBackColor = true;
+            this.rdbOff.CheckedChanged += new System.EventHandler(this.rdbOff_CheckedChanged);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(686, 353);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(102, 23);
+            this.save.TabIndex = 3;
+            this.save.Text = "salva ed esci";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(544, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(544, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(544, 145);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(544, 188);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(544, 233);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 392);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.rdbOff);
+            this.Controls.Add(this.rdbOn);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Green;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Super Ping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton rdbOn;
+        private System.Windows.Forms.RadioButton rdbOff;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Super Ping/SuperPing/SuperPing/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace SuperPing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TextBox[] txtIP = new TextBox[5];
+        TextBox[] txtStatus = new TextBox[5];
+        TextBox[] txtMs = new TextBox[5];
+        PictureBox[] boxes = new PictureBox[5];
+        string nomefile = "ip.csv", x = "X.png", v = "V.png";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader File = new StreamReader(nomefile);
+            string IPS;
+            IPS = File.ReadToEnd();
+            string[] campi = IPS.Split('%');
+
+            for (int i = 0; i < 5; i++)
+            {
+                txtIP[i] = new TextBox();
+                txtIP[i].Top = ((i + 1) * 50);
+                txtIP[i].Left = (i + 1) + 19;
+                txtIP[i].Text = campi[i];
+                this.Controls.Add(txtIP[i]);
+                txtStatus[i] = new TextBox();
+                txtStatus[i].Top = ((i + 1) * 50);
+                txtStatus[i].Left = (i + 150);
+                txtStatus[i].Text = "host";
+                this.Controls.Add(txtStatus[i]);
+                txtMs[i] = new TextBox();
+                txtMs[i].Top = ((i + 1) * 50);
+                txtMs[i].Left = (i + 300);
+                txtMs[i].Text = "ms";
+                this.Controls.Add(txtMs[i]);
+                boxes[i] = new PictureBox();
+                boxes[i].Top = ((i + 1) * 50);
+                boxes[i].Left= (i + 425);
+                boxes[i].Width = 20;
+                boxes[i].Height = 20;
+                this.Controls.Add(boxes[i]);
+            }
+            File.Close();
+        }//load
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                //this.Cursor = Cursors.WaitCursor;
+                //txtStatus[1].Text = "";
+                //txtMs.Text = "";
+                string Indirizzo = txtIP[i].Text;
+
+                Ping pinger = new Ping();
+                PingReply reply = pinger.Send(Indirizzo);
+                string status = reply.Status.ToString();
+                string millisec = reply.RoundtripTime.ToString();
+                txtStatus[i].Text = status;
+                txtMs[i].Text = millisec;
+                this.Cursor = Cursors.Arrow;
+                if (txtStatus[i].Text == "Success")
+                {
+                    txtStatus[i].BackColor = Color.LightGreen;
+                    boxes[i].Load(v);
+                }
+                else
+                {
+                    txtStatus[i].BackColor = Color.OrangeRed;
+                    boxes[i].Load(x);
+                }
+            }
+        }//bn forza 
+        
+        
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string Indirizzo = txtIP[i].Text;
+
+                Ping pinger = new Ping();
+                PingReply reply = pinger.Send(Indirizzo);
+                string status = reply.Status.ToString();
+                string millisec = reply.RoundtripTime.ToString();
+                txtStatus[i].Text = status;
+                txtMs[i].Text = millisec;
+                this.Cursor = Cursors.Arrow;
+                if (txtStatus[i].Text == "Success")
+                {
+                    txtStatus[i].BackColor = Color.LightGreen;
+                    boxes[i].Load(v);
+                }
+                else
+                {
+                    txtStatus[i].BackColor = Color.OrangeRed;
+                    boxes[i].Load(x);
+                }
+
+            }
+        }//timer
+
+        private void Name_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled=true;
+            
+        }
+
+        private void rdbOff_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter(nomefile);
+            string tds = null; 
+            for(int i=0;i<5;i++)
+            {
+                tds = tds + txtIP[i].Text + '%';
+            }
+            writer.WriteLine(tds);
+            writer.Close();
+            Application.Exit();
+        }//btn save
+    }
+}
