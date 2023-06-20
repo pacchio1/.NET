ecbb3b0ce4d75d0477793cd9a4b8f8e5f957fb4e
MarcoPacchiotti <57013700+pacchio1@users.noreply.github.com>
2023-06-20 13:47:29 +0200

    core asp

---
+++ Its/ASP.NEt/Core/Core_Ludoteca/Core_Ludoteca/Models/Gioco.cs
@@ -0,0 +1,26 @@
+﻿namespace Ludoteca.Models
+{
+    public class Gioco
+    {
+        public string Denominazione { get; set; }
+        public string Descrizione { get; set; }
+        public string Tipologia{ get; set; }
+        public string Eta_dei_giocatori { get; set; }
+        public string Numero_di_giocatori { get; set; }
+        public string Durata_partita { get; set; }
+        public int Numero_di_pezzi { get; set; }
+        public int ID_Gioco { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Denominazione)}={Denominazione}," +
+                $" {nameof(Descrizione)}={Descrizione}," +
+                $" {nameof(Tipologia)}={Tipologia}," +
+                $" {nameof(Eta_dei_giocatori)}={Eta_dei_giocatori}," +
+                $" {nameof(Numero_di_giocatori)}={Numero_di_giocatori}," +
+                $" {nameof(Durata_partita)}={Durata_partita}," +
+                $" {nameof(Numero_di_pezzi)}={Numero_di_pezzi.ToString()}," +
+                $" {nameof(ID_Gioco)}={ID_Gioco.ToString()}";
+        }
+    }
+}
---
+++ Its/ASP.NEt/Core/Core_Ludoteca/Core_Ludoteca/Models/Prestito.cs
@@ -0,0 +1,20 @@
+﻿namespace Core_Ludoteca.Models
+{
+    public class Prestito
+    {
+        public DateOnly data_prestito { get; set; }
+        public DateOnly data_restituzione { get; set; }
+        public string note { get; set; }
+        public int ID_Prestito { get; set; }
+        public int ID_Gioco { get; set; }
+        public string CF_Utente { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(data_prestito)}={data_prestito.ToString()}," +
+                $" {nameof(data_restituzione)}={data_restituzione.ToString()}," +
+                $" {nameof(note)}={note}, {nameof(ID_Prestito)}={ID_Prestito.ToString()}," +
+                $" {nameof(ID_Gioco)}={ID_Gioco.ToString()}, {nameof(CF_Utente)}={CF_Utente}";
+        }
+    }
+}
