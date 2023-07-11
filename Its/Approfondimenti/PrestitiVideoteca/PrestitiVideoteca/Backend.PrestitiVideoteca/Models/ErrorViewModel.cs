94602f2d49e16245239f8bb191cdda1935483085
MarcoPacchiotti <57013700+pacchio1@users.noreply.github.com>
2023-07-11 16:41:45 +0200

    esame finale

--- Its/Approfondimenti/PrestitiVideoteca/PrestitiVideoteca/Backend.PrestitiVideoteca/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.PrestitiVideoteca.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
